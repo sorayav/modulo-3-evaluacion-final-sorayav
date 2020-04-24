10fb953abb11dfc096a52d3a24e95adcad94ae58
Soraya Valle <svalleortega@gmail.com>
2020-04-24 17:28:51 +0200

    Destructuring

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -3,24 +3,24 @@ import icon from '../images/backhome.png';
 import { Link } from 'react-router-dom';
 
 const CharacterDetails = (props) => {
-  const episodesTotal = props.character.episode;
-  const episodesNumber = episodesTotal.length;
+  const {image, name, species, origin, episode, status} = props.character;
+
   return (
     <section className="details">
       <Link to="/" className="character__details--btn" title="Go back"><img className="home__icon" src={icon} alt="Go back" /> Go back</Link>
       <div className="character__details">
         
         <div className="character__details-img">
-          <img src={props.character.image} alt={props.character.name} />
+          <img src={image} alt={name} />
         </div>
         
       <div className="character__details--info">
-          <h2 className="character__details--name">{props.character.name}</h2>
+          <h2 className="character__details--name">{name}</h2>
           <ul className="character__details--ul">
-            <li className="character__details--species"><h3>Species:</h3> {props.character.species}</li>
-            <li className="character__details--origin"><h3>Origin:</h3> {props.character.origin.name}</li>
-            <li className="character__details--episode"><h3>Number of episodes:</h3> {episodesNumber}</li>
-            <li className="character__details--status"><h3>Status:</h3> {props.character.status}</li>
+            <li className="character__details--species"><h3>Species:</h3> {species}</li>
+            <li className="character__details--origin"><h3>Origin:</h3> {origin.name}</li>
+            <li className="character__details--episode"><h3>Number of episodes:</h3> {episode.length}</li>
+            <li className="character__details--status"><h3>Status:</h3> {status}</li>
           </ul>
         </div>
       </div>
