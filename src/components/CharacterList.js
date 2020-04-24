8dbe952b126e0b6b5c15fe45fdd800e64335b27f
Soraya Valle <svalleortega@gmail.com>
2020-04-24 13:39:32 +0200

    Fix error

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Character from './Character';
 import { Link } from 'react-router-dom';
-import ItExist from './ItExists';
+import ItExists from './ItExists'
 
 const CharacterList = (props) => {
   const {characters, searchCharacter} = props;
   const doesItExist = characters.filter(characterElement => !searchCharacter || characterElement.name.toLowerCase().includes(searchCharacter.toLowerCase()))
   
   if (doesItExist.length === 0) {
-    return <ItExist />
+    return <ItExists />
   } else {
     return ( <div> 
       <ul className="character__list">
-        {characters
+        {doesItExist
         // .sort(characterElement => characterElement.name)
         .map(characterElement =>
-          <Link key={characterElement.id} to={`character/${characterElement.id}`}>
+          <Link key={characterElement.id} to={`character/${characterElement.id}`} title="Click for more info">
           <li key = {characterElement.id}>
             <Character 
                 characterImg = {characterElement.image}
