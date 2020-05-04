a3091568e6b594cfc15beda8567ef724b555ea03
Soraya Valle <svalleortega@gmail.com>
2020-05-04 11:16:01 +0200

    Fix link

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import notFound from '../images/notfound.png';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className="character__notfound">
       <h2>There's nothing here.</h2>
-      <p>Searching is pain, you better <a href="/" title="Go home, you're drunk">go back</a> and try again.</p>
+      <p>Searching is pain, you better <Link to="/" title="Go home, you're drunk">go back</Link> and try again.</p>
       <img src={notFound} alt="Not found" />
     </div>
   )
